423afcbc5eff1061f3a4c9468f34504c10e76de5
Amaury Franssen <amauryfranssen@gmail.com>
2025-01-31 22:46:59 +0100

    feat: add some tests --wip

--- src/app/ping/ping.controller.spec.ts
+++ src/app/ping/ping.controller.spec.ts
@@ -15,6 +15,27 @@ describe('PingController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('should return pong with version info if database is running', async () => {
+    const result = await controller.ping();
+    expect(result).toBeDefined();
+    expect(result.response).toBe('pong');
+    expect(result.version).toBeDefined();
+    expect(result.version.status).toBe('OK');
+    expect(result.version.details).toBeDefined();
+    expect(result.version.details.database).toContain(`Database running - version 2`);
+  });
+
+  it('should return pong with KO as database details if database is not running', async () => {
+    const result = await controller.ping();
+    expect(result).toBeDefined();
+    expect(result.response).toBe('pong');
+    expect(result.version).toBeDefined();
+    expect(result.version.status).toBe('Partial');
+    expect(result.version.details).toBeDefined();
+    expect(result.version.details.database).toContain(`Database KO`);
+  });
+
 });
 
 
