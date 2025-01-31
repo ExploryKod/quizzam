bfc4a7c6cc46e2d55779ae8addc502b7671dcd0b
Amaury Franssen <amauryfranssen@gmail.com>
2025-01-31 21:55:33 +0100

    feat: set database config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { PingController } from 'src/ping/ping.controller';
+import { FirebaseModule } from 'nestjs-firebase';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PingController } from 'src/ping/ping.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    FirebaseModule.forRoot({
+      googleApplicationCredential: 'src/assets/quizzam-firebase-key.json',
+    }),
+  ],
   controllers: [AppController, PingController],
   providers: [AppService],
 })
 export class AppModule {}
+
+
