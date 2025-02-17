87a144706e7c43b1bbe6ef168e7c80b40df0c8da
Amaury Franssen <amauryfranssen@gmail.com>
2025-02-17 11:57:24 +0100

    feat: add middleware config from pokedex as base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
+import {   MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod
+} from '@nestjs/common';
 import { PingoController } from 'src/pingo/pingo.controller';
 import { FirebaseModule } from 'nestjs-firebase';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PingModule } from './ping/ping.module';
+import { UsersController } from './users/users.controller';
+import { AuthModule } from './modules/auth/auth.module';
+import { AuthMiddleware } from './modules/auth/auth.middleware';
 
 @Module({
   imports: [
@@ -11,10 +18,20 @@ import { PingModule } from './ping/ping.module';
     FirebaseModule.forRoot({
       googleApplicationCredential: 'src/assets/quizzam-firebase-key.json',
     }),
+    AuthModule,
   ],
-  controllers: [AppController, PingoController],
+  controllers: [AppController, PingoController, UsersController],
   providers: [AppService],
 })
-export class AppModule {}
+
+export class AppModule implements NestModule {
+  public configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
+
+
 
 
