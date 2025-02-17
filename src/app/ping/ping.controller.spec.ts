193c542a63407642bc882c4043632f1b7cee44bf
Nawaat <nawat.m12@gmail.com>
2025-02-17 12:58:01 +0100

    update tests

--- src/app/ping/ping.controller.spec.ts
+++ src/app/ping/ping.controller.spec.ts
@@ -7,9 +7,13 @@ describe('PingController', () => {
   let versionService: VersionRepositoryService;
 
   beforeEach(async () => {
+    const mockVersionService = {
+      getVersion: jest.fn(), 
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PingController],
-      providers: [VersionRepositoryService],
+      providers: [{ provide: VersionRepositoryService, useValue: mockVersionService }],
     }).compile();
 
     controller = module.get<PingController>(PingController);
@@ -22,7 +26,13 @@ describe('PingController', () => {
   });
 
   it('should return pong with version info if database is running', async () => {
+    (versionService.getVersion as jest.Mock).mockResolvedValue({
+      status: 'OK',
+      details: { database: 'Database running - version 2' },
+    });
+
     const result = await controller.ping();
+    console.log('Résultat du test (database running) :', result);
     expect(result).toBeDefined();
     expect(result.response).toBe('pong');
     expect(result.version).toBeDefined();
@@ -32,7 +42,13 @@ describe('PingController', () => {
   });
 
   it('should return pong with KO as database details if database is not running', async () => {
+    (versionService.getVersion as jest.Mock).mockResolvedValue({
+      status: 'Partial',
+      details: { database: 'Database KO' },
+    });
+
     const result = await controller.ping();
+    console.log('Résultat du test (database KO) :', result);
     expect(result).toBeDefined();
     expect(result.response).toBe('pong');
     expect(result.version).toBeDefined();
