d09387f3263e1bbedc1d8de9092a94befd8909a0
slk <sl@izydesk.fr>
2025-03-31 14:43:56 +0200

    refactor(tests): update email addresses in quiz and user tests to use 'user@test.com'

--- e2e/src/server/quiz.spec.ts
+++ e2e/src/server/quiz.spec.ts
@@ -7,7 +7,7 @@ describe('GET /api/quiz', () => {
 
   beforeAll(async () => {
     const auth = await request(defaultFirebaseUrl).post('').send({
-      email: 'user@email.com',
+      email: 'user@test.com',
       password: 'password',
       returnSecureToken: true,
     });
@@ -43,7 +43,7 @@ describe('POST /api/quiz', () => {
 
   beforeAll(async () => {
     const auth = await request(defaultFirebaseUrl).post('').send({
-      email: 'user@email.com',
+      email: 'user@test.com',
       password: 'password',
       returnSecureToken: true,
     });
@@ -52,148 +52,46 @@ describe('POST /api/quiz', () => {
     token = auth.body.idToken;
   });
 
-   it('should create a quiz successfully', async () => {
-        const quizData = {
-            title: 'Quiz Test POST /api/quiz',
-            description: 'Description du quiz test',
-        };
-        const response = await request(defaultUrl)
-            .post('/api/quiz')
-            .set('Authorization', `Bearer ${token}`)
-            .send(quizData);
+  it('should create a quiz successfully', async () => {
+    const quizData = {
+      title: 'Quiz Test POST /api/quiz',
+      description: 'Description du quiz test',
+    };
+    const response = await request(defaultUrl)
+      .post('/api/quiz')
+      .set('Authorization', `Bearer ${token}`)
+      .send(quizData);
 
-        console.log('Location header:', response.headers.location);
-        expect(response.status).toBe(201);
-        expect(response.headers).toHaveProperty('location');
-    });
+    console.log('Location header:', response.headers.location);
+    expect(response.status).toBe(201);
+    expect(response.headers).toHaveProperty('location');
+  });
 
-    it('should return 401 if user is not authenticated', async () => {
-        try {
-          await request(defaultUrl).post('/api/quiz').send({
-            email: 'user@email.com',
-            password: 'password',
-            returnSecureToken: true,
-          });
-        } catch (e) {
-          expect(e.response.status).toBe(401);
-        }
+  it('should return 401 if user is not authenticated', async () => {
+    try {
+      await request(defaultUrl).post('/api/quiz').send({
+        email: 'user@test.com',
+        password: 'password',
+        returnSecureToken: true,
       });
+    } catch (e) {
+      expect(e.response.status).toBe(401);
+    }
+  });
 });
 
 // ========== RECUPERATION D'UN QUIZ PAR ID =============== //
 describe('GET /api/quiz/:id', () => {
-    let token: string
-    let otherUserToken: string;
-    let quizId: string;
-
-    beforeAll(async () => {
-        const auth = await request(defaultFirebaseUrl).post('').send({
-          email: 'user@email.com',
-          password: 'password',
-          returnSecureToken: true,
-        });
-    
-        expect(auth.status).toBe(200);
-        token = auth.body.idToken;
-
-        // Création d'un quiz pour avoir un ID valide
-        const quizData = {
-          title: 'Quiz Test',
-          description: 'Description du quiz test',
-        };
-
-        const createResponse = await request(defaultUrl)
-          .post('/api/quiz')
-          .set('Authorization', `Bearer ${token}`)
-          .send(quizData);
-
-        expect(createResponse.status).toBe(201);
-
-        // Récupération de l'ID du quiz
-        const locationHeader = createResponse.headers.location;
-        quizId = locationHeader.split('/').pop();
-
-        const questionData = {
-          title: 'What is the capital of France?',
-          answers: [
-            { title: 'Paris', isCorrect: true },
-            { title: 'London', isCorrect: false },
-            { title: 'Rome', isCorrect: false },
-            { title: 'Berlin', isCorrect: false },
-          ],
-        };
-      
-        const questionResponse = await request(defaultUrl)
-          .post(`/api/quiz/${quizId}/questions`)
-          .set('Authorization', `Bearer ${token}`)
-          .send(questionData);
-      
-        expect(questionResponse.status).toBe(201);
-    });
-
-  it('should retrieve a quiz by ID for an authenticated user', async () => {
-
-    const response = await request(defaultUrl)
-        .get(`/api/quiz/${quizId}`)
-        .set('Authorization', `Bearer ${token}`);
-
-    console.log('Retrieved Quiz:', JSON.stringify(response.body, null, 2));
-
-    expect(response.status).toBe(200);
-    expect(response.body).toHaveProperty('title');
-    expect(response.body).toHaveProperty('description');
-    expect(response.body).toHaveProperty('questions');
-    expect(Array.isArray(response.body.questions)).toBe(true);
-
-    // Vérifie que chaque question est un objet contenant les propriétés attendues
-    response.body.questions.forEach((question) => {
-    expect(question).toHaveProperty('id');
-    expect(question).toHaveProperty('title');
-    expect(question).toHaveProperty('answers');
-    expect(Array.isArray(question.answers)).toBe(true);
-
-    // Vérifie que chaque réponse est un objet contenant `title` et `isCorrect`
-     question.answers.forEach((answer) => {
-      expect(answer).toHaveProperty('title');
-      expect(answer).toHaveProperty('isCorrect');
-      expect(typeof answer.title).toBe('string');
-      expect(typeof answer.isCorrect).toBe('boolean');
-    });
-  });
-});
-
-   it('should return 404 if the quiz does not exist', async () => {
-      const response = await request(defaultUrl)
-          .get('/api/quiz/nonexistentQuizId')
-          .set('Authorization', `Bearer ${token}`);
-      
-      expect(response.status).toBe(404);
-      });
-
-
-    it("should return 401 if the quiz doesn't belong to the authenticated user", async () => {
-        const response = await request(defaultUrl)
-        .get(`/api/quiz/${quizId}`)
-        .set('Authorization', `Bearer ${otherUserToken}`);
-
-        expect(response.status).toBe(401);
-    });
-});
-
-// ========== MODIFICATION D'UN QUIZ =============== //
-describe('PATCH /api/quiz/:id', () => {
   let token: string;
   let otherUserToken: string;
   let quizId: string;
 
   beforeAll(async () => {
-    const auth = await request(defaultFirebaseUrl)
-      .post('')
-      .send({
-        email: 'user@email.com',
-        password: 'password',
-        returnSecureToken: true,
-      });
+    const auth = await request(defaultFirebaseUrl).post('').send({
+      email: 'user@test.com',
+      password: 'password',
+      returnSecureToken: true,
+    });
 
     expect(auth.status).toBe(200);
     token = auth.body.idToken;
@@ -215,10 +113,109 @@ describe('PATCH /api/quiz/:id', () => {
     const locationHeader = createResponse.headers.location;
     quizId = locationHeader.split('/').pop();
 
+    const questionData = {
+      title: 'What is the capital of France?',
+      answers: [
+        { title: 'Paris', isCorrect: true },
+        { title: 'London', isCorrect: false },
+        { title: 'Rome', isCorrect: false },
+        { title: 'Berlin', isCorrect: false },
+      ],
+    };
+
+    const questionResponse = await request(defaultUrl)
+      .post(`/api/quiz/${quizId}/questions`)
+      .set('Authorization', `Bearer ${token}`)
+      .send(questionData);
+
+    expect(questionResponse.status).toBe(201);
+  });
+
+  it('should retrieve a quiz by ID for an authenticated user', async () => {
+    const response = await request(defaultUrl)
+      .get(`/api/quiz/${quizId}`)
+      .set('Authorization', `Bearer ${token}`);
+
+    console.log('Retrieved Quiz:', JSON.stringify(response.body, null, 2));
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('title');
+    expect(response.body).toHaveProperty('description');
+    expect(response.body).toHaveProperty('questions');
+    expect(Array.isArray(response.body.questions)).toBe(true);
+
+    // Vérifie que chaque question est un objet contenant les propriétés attendues
+    response.body.questions.forEach((question) => {
+      expect(question).toHaveProperty('id');
+      expect(question).toHaveProperty('title');
+      expect(question).toHaveProperty('answers');
+      expect(Array.isArray(question.answers)).toBe(true);
+
+      // Vérifie que chaque réponse est un objet contenant `title` et `isCorrect`
+      question.answers.forEach((answer) => {
+        expect(answer).toHaveProperty('title');
+        expect(answer).toHaveProperty('isCorrect');
+        expect(typeof answer.title).toBe('string');
+        expect(typeof answer.isCorrect).toBe('boolean');
+      });
+    });
+  });
+
+  it('should return 404 if the quiz does not exist', async () => {
+    const response = await request(defaultUrl)
+      .get('/api/quiz/nonexistentQuizId')
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(404);
+  });
+
+  it("should return 401 if the quiz doesn't belong to the authenticated user", async () => {
+    const response = await request(defaultUrl)
+      .get(`/api/quiz/${quizId}`)
+      .set('Authorization', `Bearer ${otherUserToken}`);
+
+    expect(response.status).toBe(401);
+  });
+});
+
+// ========== MODIFICATION D'UN QUIZ =============== //
+describe('PATCH /api/quiz/:id', () => {
+  let token: string;
+  let otherUserToken: string;
+  let quizId: string;
+
+  beforeAll(async () => {
+    const auth = await request(defaultFirebaseUrl).post('').send({
+      email: 'user@test.com',
+      password: 'password',
+      returnSecureToken: true,
+    });
+
+    expect(auth.status).toBe(200);
+    token = auth.body.idToken;
+
+    // Création d'un quiz pour avoir un ID valide
+    const quizData = {
+      title: 'Quiz Test',
+      description: 'Description du quiz test',
+    };
+
+    const createResponse = await request(defaultUrl)
+      .post('/api/quiz')
+      .set('Authorization', `Bearer ${token}`)
+      .send(quizData);
+
+    expect(createResponse.status).toBe(201);
+
+    // Récupération de l'ID du quiz
+    const locationHeader = createResponse.headers.location;
+    quizId = locationHeader.split('/').pop();
   });
 
   it('should update a quiz title successfully', async () => {
-    const patchOperations = [{ op: 'replace', path: '/title', value: 'New Quiz Title' }];
+    const patchOperations = [
+      { op: 'replace', path: '/title', value: 'New Quiz Title' },
+    ];
 
     const response = await request(defaultUrl)
       .patch(`/api/quiz/${quizId}`)
@@ -229,7 +226,9 @@ describe('PATCH /api/quiz/:id', () => {
   });
 
   it('should return 404 if the quiz does not exist', async () => {
-    const patchOperations = [{ op: 'replace', path: '/title', value: 'New Title' }];
+    const patchOperations = [
+      { op: 'replace', path: '/title', value: 'New Title' },
+    ];
 
     const response = await request(defaultUrl)
       .patch('/api/quiz/nonexistentQuizId')
@@ -240,7 +239,9 @@ describe('PATCH /api/quiz/:id', () => {
   });
 
   it("should return 401 if the quiz doesn't belong to the authenticated user", async () => {
-    const patchOperations = [{ op: 'replace', path: '/title', value: 'Hacked Title' }];
+    const patchOperations = [
+      { op: 'replace', path: '/title', value: 'Hacked Title' },
+    ];
 
     const response = await request(defaultUrl)
       .patch(`/api/quiz/${quizId}`)
@@ -249,7 +250,6 @@ describe('PATCH /api/quiz/:id', () => {
 
     expect(response.status).toBe(401);
   });
-
 });
 
 // ========== CREATION D'UNE QUESTION =============== //
@@ -258,13 +258,11 @@ describe('POST /api/quiz/:id/questions', () => {
   let quizId: string;
 
   beforeAll(async () => {
-    const auth = await request(defaultFirebaseUrl)
-      .post('')
-      .send({
-        email: 'user@email.com',
-        password: 'password',
-        returnSecureToken: true,
-      });
+    const auth = await request(defaultFirebaseUrl).post('').send({
+      email: 'user@test.com',
+      password: 'password',
+      returnSecureToken: true,
+    });
 
     expect(auth.status).toBe(200);
     token = auth.body.idToken;
@@ -283,7 +281,6 @@ describe('POST /api/quiz/:id/questions', () => {
 
     const locationHeader = createResponse.headers.location;
     quizId = locationHeader.split('/').pop();
-
   });
 
   it('should add a question to a quiz successfully', async () => {
@@ -296,7 +293,7 @@ describe('POST /api/quiz/:id/questions', () => {
         { title: 'Berlin', isCorrect: false },
       ],
     };
-    
+
     console.log(quizId);
     const response = await request(defaultUrl)
       .post(`/api/quiz/${quizId}/questions`)
@@ -326,7 +323,6 @@ describe('POST /api/quiz/:id/questions', () => {
 
     expect(response.status).toBe(404);
   });
-
 });
 
 // ========== DEMARRAGE D'UN QUIZ =============== //
@@ -334,8 +330,8 @@ describe('POST /api/quiz/:id/start', () => {
   let token: string;
   let nonexistentQuizId: string;
   let quizId: string;
-  
-  const currentUser = 'user@email.com';
+
+  const currentUser = 'user@test.com';
   const currentPassword = 'password';
 
   beforeAll(async () => {
@@ -352,7 +348,7 @@ describe('POST /api/quiz/:id/start', () => {
       title: 'Quiz Test start',
       description: 'Description du quiz test',
     };
-    
+
     const createResponse = await request(defaultUrl)
       .post('/api/quiz')
       .set('Authorization', `Bearer ${token}`)
@@ -363,23 +359,23 @@ describe('POST /api/quiz/:id/start', () => {
     const locationHeader = createResponse.headers.location;
     quizId = locationHeader.split('/').pop();
 
-     // Ajout d'une question au quiz
-  const questionData = {
-    title: 'What is the capital of France?',
-    answers: [
-      { title: 'Paris', isCorrect: true },
-      { title: 'London', isCorrect: false },
-      { title: 'Rome', isCorrect: false },
-      { title: 'Berlin', isCorrect: false },
-    ],
-  };
+    // Ajout d'une question au quiz
+    const questionData = {
+      title: 'What is the capital of France?',
+      answers: [
+        { title: 'Paris', isCorrect: true },
+        { title: 'London', isCorrect: false },
+        { title: 'Rome', isCorrect: false },
+        { title: 'Berlin', isCorrect: false },
+      ],
+    };
 
-  const questionResponse = await request(defaultUrl)
-    .post(`/api/quiz/${quizId}/questions`)
-    .set('Authorization', `Bearer ${token}`)
-    .send(questionData);
+    const questionResponse = await request(defaultUrl)
+      .post(`/api/quiz/${quizId}/questions`)
+      .set('Authorization', `Bearer ${token}`)
+      .send(questionData);
 
-  expect(questionResponse.status).toBe(201);
+    expect(questionResponse.status).toBe(201);
   });
 
   it('should start an existing quiz successfully with a correct pattern for its id', async () => {
@@ -400,7 +396,6 @@ describe('POST /api/quiz/:id/start', () => {
 
     expect(response.status).toBe(404);
   });
-
 
   it('should return 400 if quiz is not ready to be started', async () => {
     // Création d'un quiz sans questions
@@ -426,5 +421,4 @@ describe('POST /api/quiz/:id/start', () => {
 
     expect(response.status).toBe(400);
   });
-
 });
