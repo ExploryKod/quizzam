8bbd5416a28640ff96d3d07f23a6512fe1ae1136
Amaury Franssen <amauryfranssen@gmail.com>
2025-04-03 22:29:52 +0200

    docs: add css and template ejs file to make a tiny welcom page

--- src/core/app.controller.ts
+++ src/core/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getData() {
-    return this.appService.getData();
+  @Get('/')
+  @Render('index')
+  getHomePage() {
+    return {
+      title: this.appService.getWelcomTexts().title,
+      description: this.appService.getWelcomTexts().description,
+      btnText: this.appService.getBtnTexts().docs,
+    };
   }
 }
--- src/core/app.service.ts
+++ src/core/app.service.ts
@@ -2,7 +2,16 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return ({ message: 'Hello API' });
+  getWelcomTexts(): { title: string, description: string } {
+    return ({
+      title: 'Bienvenue sur l\'API Quizzy',
+      description: 'Professeurs, créez vos quiz avec cette api sur-mesure. Swagger est utilisé pour la documentation des routes.'
+    });
+
+  }
+  getBtnTexts(): { docs: string } {
+    return ({
+      docs: 'Consulter les docs (swagger)'
+    });
   }
 }
