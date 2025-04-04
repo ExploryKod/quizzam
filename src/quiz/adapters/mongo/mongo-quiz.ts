56e607248fc2be2b05c72c3a6c7ef2b410e7aec7
Amaury Franssen <amauryfranssen@gmail.com>
2025-04-04 17:16:03 +0200

    feat(types): prepare new organisation of types

--- src/quiz/adapters/mongo/mongo-quiz.ts
+++ src/quiz/adapters/mongo/mongo-quiz.ts
@@ -4,42 +4,6 @@ import {
   SchemaFactory,
 } from '@nestjs/mongoose';
 import { HydratedDocument, Schema as MongooseBaseSchema } from 'mongoose';
-// type Answer = {
-//   isCorrect: boolean;
-//   title: string;
-// }
-//
-// type Question = {
-//   id: string;
-//   title: string;
-//   answers: Answer[];
-// }
-//
-// export namespace MongoQuiz {
-//   export const CollectionName = 'quizzes';
-//
-//   @MongooseSchema({ collection: CollectionName })
-//   export class SchemaClass {
-//     @Prop({ type: String })
-//     _id: string;
-//
-//     @Prop()
-//     title: string;
-//
-//     @Prop()
-//     description: string;
-//
-//     @Prop()
-//     questions: Array<Question>;
-//
-//     @Prop()
-//     userId: string;
-//   }
-//
-//   export const Schema = SchemaFactory.createForClass(SchemaClass);
-//   export type Document = HydratedDocument<SchemaClass>;
-// }
-
 
 type Answer = {
   isCorrect: boolean;
